e29c6495281ee8929db56645e206aa2b68bd6da7
Dora <55266839@qq.com>
2020-12-15 13:34:35 +0800

    设置header

--- main.go
+++ main.go
@@ -6,14 +6,13 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "这里是首页")
+		fmt.Fprint(w, "这里是首页 <a href=\"/about\">关于</a>")
 	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "这里是关于页面")
+		fmt.Fprint(w, "这里是关于页面 <a href=\"/\">首页</a>")
 	} else {
 		fmt.Fprint(w, "页面未找到")
-
 	}
 }
 
