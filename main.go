03b1c94ae67e99e2d03cb8956134946c72ec3774
Dora <55266839@qq.com>
2020-12-15 14:00:00 +0800

    尝试ServeMux

--- main.go
+++ main.go
@@ -5,21 +5,28 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "这里是首页 <a href=\"/about\">关于</a>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "这里是关于页面 <a href=\"/\">首页</a>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "页面未找到")
+		fmt.Fprint(w, "404 NOT FOUND")
 	}
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "这里是关于页面 <a href=\"/\">首页</a>")
 }
 
 func main() {
 	fmt.Println("http://localhost:3000")
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+
+	http.ListenAndServe(":3000", router)
 
 }
