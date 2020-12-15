553add494f21b7c7ec430d963cdbb40f74829b45
Dora <55266839@qq.com>
2020-12-15 13:13:49 +0800

    新增关于页面

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dora-exku/blog
+
+go 1.15
--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	fmt.Fprint(w, "路径:"+r.URL.Path)
 }
 func main() {
+	fmt.Println("http://localhost:3000")
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
+
 }
