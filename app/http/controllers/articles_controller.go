fe367bd89c4fdf394ce89d4f2009de2dad376117
Dora <55266839@qq.com>
2021-01-07 13:38:02 +0800

    切割模版

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -9,6 +9,7 @@ import (
 	"goblog/pkg/types"
 	"html/template"
 	"net/http"
+	"path/filepath"
 	"strconv"
 	"unicode/utf8"
 
@@ -52,9 +53,14 @@ func (*ArticlesController) Index(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(w, "500 server error")
 	} else {
-		tmpl, err := template.ParseFiles("resources/views/articles/index.gohtml")
+		viewDir := "resources/views"
+
+		files, err := filepath.Glob(viewDir + "/layouts/*.gohtml")
 		logger.LogError(err)
-		tmpl.Execute(w, articles)
+		newFiles := append(files, viewDir+"/articles/index.gohtml")
+		tmpl, err := template.ParseFiles(newFiles...)
+		logger.LogError(err)
+		tmpl.ExecuteTemplate(w, "app", articles)
 	}
 }
 
